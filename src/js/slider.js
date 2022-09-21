8a818fe69804c909806bcd63433d1996f7bc39af
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-09-21 17:20:49 +0300

    add fix

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+import "slick-carousel";
+
+$(".gallery-bottom-slider").slick({
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 30000,
+});
+
+$(".products-list").slick({});
